955bfc82db248ee782675a079075c84fac979152
solarconnectbpo-creator <solarconnectbpo@gmail.com>
2026-03-21 14:24:44 -0500

    Add useReportGenerator hook

---
+++ src/hooks/useReportGenerator.js
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+
+export const useReportGenerator = () => {
+    const [reportData, setReportData] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
+    
+    const generateReport = async (params) => {
+        setIsLoading(true);
+        // Logic to generate the report
+        // Simulated async call
+        setTimeout(() => {
+            setReportData({ /* generated report data */ });
+            setIsLoading(false);
+        }, 1000);
+    };
+    
+    return { reportData, isLoading, generateReport };
+};
